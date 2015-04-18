64f641addaa36913d8c46dc1d9c1ccf2d7715d0b
Paul Murphy <paul@clarify.io>
2015-04-18 17:04:03 -0500

    Added library dependencies.

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ history = open('HISTORY.rst').read().replace('.. :changelog:', '')
 
 setup(
     name='clarify_python',
-    version='2.0.0',
+    version='1.1.1',
     description='The Clarify Python 3 Helper Library wraps the entire Clarify API in Python 3.x function calls.',
     long_description=readme + '\n\n' + history,
     author='Paul Murphy',
@@ -32,6 +32,8 @@ setup(
                  'clarify_python'},
     include_package_data=True,
     install_requires=[
+         'urllib3',
+         'certifi'
     ],
     license="MIT",
     zip_safe=False,
